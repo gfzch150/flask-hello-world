9a3a32215c3dd8c134336b082d56794ec63de0c1
gfzch150 <112061801+gfzch150@users.noreply.github.com>
2023-01-27 11:34:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,90 +38,51 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mtext = event.message.text
-    if mtext == '@傳送文字':
-        try:
-            message = TextSendMessage(  
-                text = "我是 Linebot，\n您好！"
-            )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    if mtext == '鉅亨':
+        sendUse(event)
+    elif mtext == '工商時報' or mtext =='t':
+        sendcommercial(event)
+    elif mtext == '天氣':
+        test(event)
         
-    elif mtext == '@傳送圖片':
-        try:
-            message = ImageSendMessage(
-                original_content_url = "https://i.imgur.com/4QfKuz1.png",
-                preview_image_url = "https://i.imgur.com/4QfKuz1.png"
-            )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
-    elif mtext == '@傳送貼圖':
-        try:
-            message = StickerSendMessage(  #貼圖兩個id需查表
-                package_id='1',  
-                sticker_id='2'
-            )
-            line_bot_api.reply_message(event.reply_token, message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
-    elif mtext == '@多項傳送':
-        try:
-            message = [  #串列
-                StickerSendMessage(  #傳送貼圖
-                    package_id='1',  
-                    sticker_id='2'
-                ),
-                TextSendMessage(  #傳送文字
-                    text = "這是 Pizza 圖片！"
-                ),
-                ImageSendMessage(  #傳送圖片
-                    original_content_url = "https://i.imgur.com/4QfKuz1.png",
-                    preview_image_url = "https://i.imgur.com/4QfKuz1.png"
-                )
-            ]
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
-    elif mtext == '@傳送位置':
-        try:
-            message = LocationSendMessage(
-                title='101大樓',
-                address='台北市信義路五段7號',
-                latitude=25.034207,  #緯度
-                longitude=121.564590  #經度
-            )
-            line_bot_api.reply_message(event.reply_token, message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
-    if mtext == '@快速選單':
-        try:
-            message = TextSendMessage(
-                text='請選擇最喜歡的程式語言',
-                quick_reply=QuickReply(
-                    items=[
-                        QuickReplyButton(
-                            action=MessageAction(label="Python", text="Python")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="Java", text="Java")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="C#", text="C#")
-                        ),
-                        QuickReplyButton(
-                            action=MessageAction(label="Basic", text="Basic")
-                        ),
-                    ]
-                )
-            )
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
+   
+   
+        
+def sendUse(event):
+    try:
+        base = "https://news.cnyes.com"
+        url  = "https://news.cnyes.com/news/cat/headline"
+        re   = requests.get(url)
+        content = ""
+        soup = BeautifulSoup(re.text, "html.parser")
+        data = soup.find_all("a", {"class": "_1Zdp"})
+        for index, d in enumerate(data):
+           if index <6:
+            title = d.text
+            href  = base + d.get("href")
+            content += "{}\n{}\n".format(title, href) #格式化函數
+            
+            message =  content
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='家裡死人'))
+def sendcommercial(event):
+    try:
+        
+        url  = "https://ctee.com.tw"
+        re   = requests.get(url)
+        content = ""
+        soup = BeautifulSoup(re.text, "html.parser")
+        data = soup.find_all("a", {"class": "post-title post-url"})
+        for index, d in enumerate(data):
+           if index <6:
+            title = d.text
+            href  =   d.get("href")
+            content += "{}\n{}\n".format(title, href) #格式化函數
+            
+            message =  content
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=message))
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='家裡死人'))
 if __name__ == "__main__":
     app.run()
